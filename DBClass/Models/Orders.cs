0c44066534774db6fd28a4dc350e5c419d4c5434
Brovu <roy772003@gmail.com>
2023-09-28 19:34:40 +0700

    Buoi 2

---
+++ DBClass/Models/Orders.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DBClass.Models
+{
+    [Table("Orders")]
+    public class Orders
+    {
+        [Key]
+        public int Id { get; set; }
+        public int UserId { get; set; }
+
+        [Required]
+        public string ReceiverAddress { get; set; }
+        public string ReceiverEmail { get; set; }
+        public string ReceiverPhone { get; set; }
+
+        public string Note { get; set; }
+
+        [Required]
+        public DateTime CreateAt { get; set; }
+        public string UpdateBy { get; set; }
+        public DateTime UpdateAt { get; set; }
+        public int Status { get; set; }
+
+
+    }
+}
---
+++ Web-ThucHanh/Controllers/SiteController.cs
@@ -0,0 +1,20 @@
+﻿using DBClass.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Web_ThucHanh.Controllers
+{
+    public class SiteController : Controller
+    {
+        public ActionResult Index()
+        {
+            
+            return View();
+        }
+
+        
+    }
+}
